600647002a3c018b0a1ff9bb2e0fe0e454b81892
Dhananjay Sonar <126058839+DhananjaySonar136@users.noreply.github.com>
2026-04-29 23:40:50 +0530

    Add files via upload

---
+++ looping_statements/while_loop.java
@@ -0,0 +1,56 @@
+package looping_statements;
+
+import java.util.Scanner;
+
+public class while_loop {
+
+	public static void main(String[] args) {
+
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter pin:");
+//		int pin=sc.nextInt();
+//		
+//		while(pin>0) {
+//			if(pin==1212) {
+//				System.out.println("Correct");
+//				break;
+//			}
+//			else {
+//				System.out.println("Enter correct pin:");
+//				pin=sc.nextInt();
+//			}
+//		}
+		
+		
+		
+		
+//		int pin=1212;
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter pin:");
+//		int userpin=sc.nextInt();
+//		int attempts=0;
+//		while(attempts<3) {
+//			if(userpin==pin) {
+//				System.out.println("Login....");
+//				break;
+//			}
+//			else {
+//				attempts++;
+//				if(attempts==3) {
+//					System.out.println("your account blocked!!!");
+//					break;
+//				}
+//				
+//				System.out.println("incorrect pin!!! attempts left:"+(3-attempts)+"enter pin :");
+//				userpin=sc.nextInt();
+//				
+//			}
+//		}
+		
+			
+		
+		
+		
+	}
+
+}
